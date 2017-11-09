1821425997c3ef9abf2112b7aaf64265ba372d4d
JJPaccito <33262487+JJPaccito@users.noreply.github.com>
2017-11-09 19:07:44 +0200

    Start

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bakery</title>
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+</head>
+<body>
+	<table id="prekes">
+		<thead id='head'>
+			<td>Pavadinimas</td>
+		</thead>
+			<tbody>
+				<tr>
+					<td>Agouninė</td>
+				</tr>
+				<tr>
+					<td>Varškės</td>
+				</tr>
+				<tr>
+					<td>Duona balta</td>
+				</tr>
+				<tr>
+					<td>Duona juoda</td>
+				</tr>
+				<tr>
+					<td>Duona ruginė</td>
+				</tr>
+			</tbody>
+	</table>
+</body>
+</html>
