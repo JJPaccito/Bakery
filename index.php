20460359bde8bf4327582b6354882a37bf53c4fe
JJPaccito <33262487+JJPaccito@users.noreply.github.com>
2017-11-09 21:27:55 +0200

    fix

--- index.php
+++ index.php
@@ -59,6 +59,11 @@
 					<td>19</td>
 					<td>4</td>
 					<td>2</td>
+					<td>5</td>
+					<td>20</td>
+					<td>19</td>
+					<td>4</td>
+					<td>2</td>
 				</tr>
 				<tr>
 					<td>Duona juoda</td>
